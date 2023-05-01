65e1745f4cb472141fce1e265d93ca4b7812152e
YuQing (Scott) <scottdingyuqing@outlook.com>
2023-05-01 12:02:40 -0300

    Fixed some Regular expression bugs

--- Outcomes2Excel/OO2Excel.py
+++ Outcomes2Excel/OO2Excel.py
@@ -4,16 +4,23 @@ import glob
 import pdfplumber
 import pandas as pd
 
+def extract_course_info(file_name):
+    course_info = os.path.basename(file_name).split('.')[0]
+    course_info = course_info.replace('_', ':')
+    return course_info
+
 def process_pdf(file_name):
     text = ""
     with pdfplumber.open(file_name) as pdf:
         for page in pdf.pages:
             text += page.extract_text()
 
-    file_basename = os.path.basename(file_name)
-    first_four_letters = file_basename[:4]
-    text = re.sub(r'(\d+\. .*?\.)\n\d+ ' + re.escape(first_four_letters) + r' .*?- \d+', r'\1', text)
-    text = re.sub(r'^(\d+\. .*?)\n\d+ ', r'\1\n', text, flags=re.MULTILINE)
+    course_info = extract_course_info(file_name)
+    page_header_regex = re.compile(rf"{re.escape(course_info)} - \d+")
+    page_footer_regex = re.compile(rf"\d+\s+{re.escape(course_info)}")
+    text = page_header_regex.sub('', text)
+    text = page_footer_regex.sub('', text)
+    text = text.replace("Copyright statement", "")
 
     outcomes_and_objectives = re.search(
         r'Course Outcomes and Objectives([\s\S]*?)(Other Course Notes:|$)',
@@ -25,12 +32,15 @@ def process_pdf(file_name):
 
     outcomes_and_objectives = outcomes_and_objectives.group(1)
     outcomes = re.findall(r'Outcome\s*?\n(\d+\. .*?)\nObjectives', outcomes_and_objectives, re.IGNORECASE)
+
     objectives = re.findall(r'Objectives([\s\S]*?)(?=\nIn keeping with|$)', outcomes_and_objectives, re.IGNORECASE)
 
     cleaned_objectives = []
     for obj_list in objectives:
-        cleaned_obj_list = re.findall(r'\d+\. .*?(?=\n\d+\. |\n\Z|$)', obj_list)
-        formatted_obj_list = [f"{i + 1}:{obj[obj.index('.')+2:]}" for i, obj in enumerate(cleaned_obj_list)]
+        obj_list = re.sub(r'\n(\d+\. )', r'\n***\1', obj_list)  # Replace line breaks before objectives with a delimiter
+        obj_list = obj_list.replace('\n', ' ').strip()  # Remove line breaks within objectives
+        cleaned_obj_list = re.findall(r'\d+\. .*?(?=\*\*\*\d+\. |\Z)', obj_list)  # Split objectives using the delimiter
+        formatted_obj_list = ["{}:{}".format(i + 1, obj[obj.index('.')+2:]) for i, obj in enumerate(cleaned_obj_list)]
         cleaned_objectives.append(formatted_obj_list)
 
     data = {
@@ -40,7 +50,7 @@ def process_pdf(file_name):
     }
     for i, (outcome, objective_list) in enumerate(zip(outcomes, cleaned_objectives)):
         for j, objective in enumerate(objective_list):
-            data['Course#'].append(file_basename.split('.')[0])
+            data['Course#'].append(course_info)
             data['Outcome'].append(outcome)
             data['Objectives'].append(objective)
 
